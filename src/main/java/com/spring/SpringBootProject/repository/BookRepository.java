515970642f8593cadb36508019b4b8aa5c629430
Shainky Jain <jainshainky123@gmail.com>
2024-03-20 00:10:06 +0530

    initial Commit

---
+++ src/main/java/com/spring/SpringBootProject/repository/BookRepository.java
@@ -0,0 +1,10 @@
+package com.spring.SpringBootProject.repository;
+
+import com.spring.SpringBootProject.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Integer> {
+
+}
---
+++ src/main/java/com/spring/SpringBootProject/repository/MyBookRepository.java
@@ -0,0 +1,9 @@
+package com.spring.SpringBootProject.repository;
+
+import com.spring.SpringBootProject.entity.MyBookList;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MyBookRepository extends JpaRepository<MyBookList, Integer> {
+}
